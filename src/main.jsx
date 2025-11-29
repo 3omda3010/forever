60f86ef96aec504ca78c1ce6cd91295f7769d591
3omda <memad7628@gmail.com>
2025-11-30 00:50:17 +0200

    feat: Initialize React application with routing and global shop context.

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/forever">
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
