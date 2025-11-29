d7873081fb2dc54545e88c5e16c4b3e553d33c15
3omda <memad7628@gmail.com>
2025-11-30 01:03:38 +0200

    feat: Set up React application with BrowserRouter and configure base path for GitHub Pages deployment.

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/forever">
+  <BrowserRouter basename="/forever/">
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
